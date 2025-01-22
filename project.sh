20bcd526eef8107238c9f3ccac3b2aad4c59e3e8
Khaledgaafar <khaledgafaar@gmail.com>
2025-01-22 15:26:07 +0200

    Farah and Khaled Commit

--- project.sh
+++ project.sh
@@ -2,8 +2,7 @@
 shopt -s extglob
 
 
-                                 #-------------main Menu ----------------------------
-                                 
+#-------------main Menu ----------------------------                                 
 Mainmenu() {
    echo  -e "\n+------------MainMenu------------+"
    echo "| 1. connectDB                  |"
@@ -31,8 +30,7 @@ Mainmenu() {
  }
  
  
-                                    #-----------------cnnect database --------------------------- 
-                                    
+#-----------------cnnect database ---------------------------                                     
 connectDB() {
    read -p "please enter name of DB " nameDB
    cd ./disk/$nameDB 2>/dev/null
@@ -47,8 +45,7 @@ connectDB() {
    fi
 }
 
-                                     #------------------create database -----------------------------
-                                     
+#------------------create database -----------------------------                                     
 createDB (){
 read -p "enter the name of your dabase name: " dbname
     if [[ $dbname == "" ]]
@@ -62,15 +59,13 @@ read -p "enter the name of your dabase name: " dbname
     fi
 }
 
-                                    #--------------------list databases------------------------------------
-                                    
+#--------------------list databases------------------------------------                                    
 listDB (){
     ls ~/disk
 }
 
 
-                                   #---------------------drop databases-------------------------------------
-                                   
+#---------------------drop databases-------------------------------------                                   
 function dropDB {
     read -p "please enter name of droppedDb " nameDB
     rm -r ./disk/$nameDB 2>/dev/null
@@ -84,8 +79,7 @@ function dropDB {
    Mainmenu
 }
 
-                                 #---------------------menu table-----------------------------------------------
-
+#---------------------menu table-----------------------------------------------
 function menuTable {
    echo  -e "\n+------------Maintable------------+"
    echo "| 1. create Table                 |"
@@ -95,9 +89,83 @@ function menuTable {
    echo "| 5. select from Table            |"
    echo "| 6. delete from Table            |"
    echo "| 7. update Table                 |"
+   
+   read -p "please select choice" choise
+   case $choise in 
+           1)
+             createTable
+             ;;
+         
+   esac
 }
    
-    
+#--------------------------------craete table ------------------------
+function createTable {
+  echo -e "Table Name: \c"
+  read tableName
+  if [[ -e $tableName ]]; then
+    echo "table already existed ,choose another name"
+    menuTable
+  fi
+  echo -e "Number of Columns: \c"
+  read colsNum
+  counter=1
+  sep="|"
+  fSep="\n"
+  pKey=""
+  metaData="Field"$sep"Type"$sep"key"
+  while [ $counter -le $colsNum ]
+  do
+    read -p "Name of Column No.$counter: " colName
+   
+    echo -e "Type of Column $colName: "
+    select var in "int" "str"
+    do
+      case $var in
+        int ) colType="int";break;;
+        str ) colType="str";break;;
+        * ) echo "Wrong Choice" ;;
+      esac
+    done
+    if [[ $pKey == "" ]]; then
+      echo -e "Make PrimaryKey ? "
+      select var in "yes" "no"
+      do
+        case $var in
+          yes ) pKey="PK";
+          metaData+=$fSep$colName$sep$colType$sep$pKey;
+          break;;
+          no )
+          metaData+=$fSep$colName$sep$colType$sep""
+          break;;
+          * ) echo "Wrong Choice" ;;
+        esac
+      done
+      else
+      metaData+=$fSep$colName$sep$colType$sep""
+    fi
+    if [[ $counter == $colsNum ]]; 
+    then
+      temp=$temp$colName
+    else
+      temp=$temp$colName$sep
+    fi
+    ((counter++))
+  done
+  touch .$tableName
+  echo -e $metaData  >> .$tableName
+  touch $tableName
+  echo -e $temp >> $tableName
+  if [[ $? == 0 ]]
+  then
+    echo "Table Created Successfully"
+    menuTable
+  else
+    echo "Error Creating Table $tableName"
+    menuTable
+  fi
+}
+
 
 Mainmenu
    
